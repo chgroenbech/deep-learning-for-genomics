956596fe645bd40ee5b52cb98c192f94e9f2188b
Christopher Heje Grønbech <chgroenbech@gmail.com>
2016-11-02 14:09:48 +0100

    Change output of model
    Take the logarithm of the secondary output of the model, and change
    other code which uses the secondary output accordingly.

--- src/modeling.py
+++ src/modeling.py
@@ -68,32 +68,36 @@ class VAE(object):
             l_dec = DenseLayer(l_dec, num_units = hidden_size, nonlinearity = rectify, name = 'DEC_DENSE{:d}'.format(len(hidden_structure) - i))
         
         l_x_p = DenseLayer(l_dec, num_units = feature_shape, nonlinearity = sigmoid, name = 'DEC_X_P')
-        l_x_r = DenseLayer(l_dec, num_units = feature_shape, nonlinearity = lambda x: affine_rectify(x, b = 1), name = 'DEC_X_R')
+        l_x_log_r = DenseLayer(l_dec, num_units = feature_shape, nonlinearity = sigmoid, name = 'DEC_X_LOG_R')
         
-        self.decoder = {"p": l_x_p, "r": l_x_r}
+        self.decoder = {"p": l_x_p, "log_r": l_x_log_r}
         
         ## Get outputs from models
         
         ## Training outputs
-        z_train, z_mu_train, z_log_var_train = get_output([l_z, l_z_mu, l_z_log_var], {l_enc_in: symbolic_x}, deterministic = False)
-        x_p_train, x_r_train = get_output([l_x_p, l_x_r], {l_dec_in: z_train}, deterministic = False)
+        z_train, z_mu_train, z_log_var_train = get_output(
+            [l_z, l_z_mu, l_z_log_var], {l_enc_in: symbolic_x}, deterministic = False)
+        x_p_train, x_log_r_train = get_output(
+            [l_x_p, l_x_log_r], {l_dec_in: z_train}, deterministic = False)
         
         ## Evaluation outputs
-        z_eval, z_mu_eval, z_log_var_eval = get_output([l_z, l_z_mu, l_z_log_var], {l_enc_in: symbolic_x}, deterministic = True)
-        x_p_eval, x_r_eval = get_output([l_x_p, l_x_r], {l_dec_in: z_eval}, deterministic = True)
+        z_eval, z_mu_eval, z_log_var_eval = get_output(
+            [l_z, l_z_mu, l_z_log_var], {l_enc_in: symbolic_x}, deterministic = True)
+        x_p_eval, x_log_r_eval = get_output(
+            [l_x_p, l_x_log_r], {l_dec_in: z_eval}, deterministic = True)
         
         ## Sample outputs
         x_p_sample = get_output(l_x_p, {l_dec_in: symbolic_z},
             deterministic = True)
-        x_r_sample = get_output(l_x_r, {l_dec_in: symbolic_z},
+        x_log_r_sample = get_output(l_x_log_r, {l_dec_in: symbolic_z},
             deterministic = True)
         
         # Likelihood
         
-        LL_train, logpx_train, KL_train = self.logLikelihood(x_p_train, x_r_train, symbolic_x, z_mu_train, z_log_var_train)
-        LL_eval, logpx_eval, KL_eval = self.logLikelihood(x_p_eval, x_r_eval, symbolic_x, z_mu_eval, z_log_var_eval)
+        LL_train, logpx_train, KL_train = self.logLikelihood(x_p_train, x_log_r_train, symbolic_x, z_mu_train, z_log_var_train)
+        LL_eval, logpx_eval, KL_eval = self.logLikelihood(x_p_eval, x_log_r_eval, symbolic_x, z_mu_eval, z_log_var_eval)
 
-        all_params = get_all_params([l_z, l_x_p, l_x_r], trainable = True)
+        all_params = get_all_params([l_z, l_x_p, l_x_log_r], trainable = True)
         
         print("Parameters to train:")
         for parameter in all_params:
@@ -103,7 +107,7 @@ class VAE(object):
         all_grads = T.grad(-LL_train, all_params)
 
         # Set the update function for parameters. The Adam optimizer works really well with VAEs.
-        update_expressions = updates.adam(all_grads, all_params, learning_rate = 1e-3)
+        update_expressions = updates.adam(all_grads, all_params, learning_rate = 1e-2)
 
         self.f_train = theano.function(inputs = [symbolic_x],
                                   outputs = [LL_train, logpx_train, KL_train],
@@ -116,10 +120,10 @@ class VAE(object):
                                  outputs = [z_eval])
 
         self.f_sample = theano.function(inputs = [symbolic_z],
-                                 outputs = [x_p_sample, x_r_sample])
+                                 outputs = [x_p_sample, x_log_r_sample])
 
         self.f_recon = theano.function(inputs = [symbolic_x],
-                                 outputs = [x_p_eval, x_r_eval])
+                                 outputs = [x_p_eval, x_log_r_eval])
     
     def train(self, x_train, x_valid = None, N_epochs = 50, batch_size = 100):
         
@@ -156,10 +160,6 @@ class VAE(object):
                 
                 print(", {:.3g} (validation set)".format(int(out[0])), end = "")
                 
-                # z_eval = self.f_z(x_valid)[0]
-                # x_p_sample, x_r_sample = self.f_sample(numpy.random.normal(size = (100, self.latent_size)).astype('float32'))#[0]
-                # x_p_recon, x_r_recon = self.f_recon(x_valid)#[0]
-            
             print(".")
     
     def save(self, name):
@@ -168,7 +168,7 @@ class VAE(object):
             "encoder": get_all_param_values(self.encoder),
             "decoder": {
                 "p": get_all_param_values(self.decoder["p"]),
-                "r": get_all_param_values(self.decoder["r"])
+                "log_r": get_all_param_values(self.decoder["log_r"])
             }
         }
         
@@ -183,29 +183,33 @@ class VAE(object):
         set_all_param_values(self.encoder, parameter_value_sets["encoder"])
         set_all_param_values(self.decoder["p"],
             parameter_value_sets["decoder"]["p"])
-        set_all_param_values(self.decoder["r"],
-            parameter_value_sets["decoder"]["r"])
+        set_all_param_values(self.decoder["log_r"],
+            parameter_value_sets["decoder"]["log_r"])
     
     def evaluate(self, x_test):
         LL_test, _, _ = self.f_eval(x_test)
         z_eval = self.f_z(x_test)
-        x_p_sample, x_r_sample = self.f_sample(numpy.random.normal(size = (100, self.latent_size)).astype('float32'))#[0]
-        x_p_recon, x_r_recon = self.f_recon(x_test)#[0]
+        x_p_sample, x_log_r_sample = self.f_sample(numpy.random.normal(size = (100, self.latent_size)).astype('float32'))#[0]
+        x_p_recon, x_log_r_recon = self.f_recon(x_test)#[0]
         
-        results = {
+        metrics = {
             "LL_test": LL_test
         }
         
-        return results
+        x_sample = meanOfNegativeBinimialDistribution(x_p_sample, x_log_r_sample)
+        x_test_recon = meanOfNegativeBinimialDistribution(x_p_recon, x_log_r_recon)
+        
+        return x_test_recon, x_sample, metrics
     
-    def logLikelihood(self, x_p, x_r, x, z_mu, z_log_var):
+    def logLikelihood(self, x_p, x_log_r, x, z_mu, z_log_var):
         #note that we sum the latent dimension and mean over the samples
-        log_px_given_z = log_negative_binomial(x, x_p, x_r, eps = 1e-6).sum(axis = 1).mean()
+        log_px_given_z = log_negative_binomial(x, x_p, x_log_r, eps = 1e-6).sum(axis = 1).mean()
+        # TODO Add normalisation in model.
         KL_qp = kl_normal2_stdnormal(z_mu, z_log_var).sum(axis = 1).mean()
         LL = - KL_qp + log_px_given_z
         return LL, log_px_given_z, KL_qp
 
-def log_poisson_gamma(x, r, p, eps = 0.0, approximation = "simple"):
+def log_negative_binomial(x, log_r, p, eps = 0.0, approximation = "simple"):
     """
     Compute log pdf of a negative binomial distribution with success probability p and number of failures, r, until the experiment is stopped, at values x.
     
@@ -215,38 +219,14 @@ def log_poisson_gamma(x, r, p, eps = 0.0, approximation = "simple"):
     x = T.clip(x, eps, x)
     
     p = T.clip(p, eps, 1.0 - eps)
-    r = T.clip(r, eps, r)
+    log_r = T.clip(log_r, eps, log_r)
     
-    if approximation == "simple":
-        def stirling(x):
-            if x == 0:
-                return 0
-            else:
-                return x * T.log(x) - x
+    r = T.exp(log_r)
     
-    y = stirling(r + x) - stirling(x) - stirling(r) + x * T.log(p) + r * T.log(1-p)
-    
-    return y
-
-def log_negative_binomial(x, r, p, eps = 0.0, approximation = "simple"):
-    """
-    Compute log pdf of a negative binomial distribution with success probability p and number of failures, r, until the experiment is stopped, at values x.
-    
-    A simple variation of Stirling's approximation is used: log x! = x log x - x.
-    """
-    
-    x = T.clip(x, eps, x)
-    
-    p = T.clip(p, eps, 1.0 - eps)
-    r = T.clip(r, 1 + eps, r)
-    
-    if approximation == "simple":
-        stirling = lambda x: x * T.log(x) - x
-    
-    y = stirling(x + r - 1) - stirling(x) - stirling(r - 1) \
+    y = T.gammaln(x + r) - T.gammaln(x + 1) - T.gammaln(r) \
         + x * T.log(p) + r * T.log(1 - p)
     
     return y
 
-def affine_rectify(x, a = 1, b = 0):
-    return a * rectify(x) + b
+def meanOfNegativeBinimialDistribution(p, log_r):
+    return p * numpy.exp(log_r) / (1 - p)
