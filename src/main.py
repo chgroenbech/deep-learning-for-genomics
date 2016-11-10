51db8507edd9157adf980e58fec6a414f8496b70
Christopher Heje Grønbech <chgroenbech@gmail.com>
2016-11-10 17:09:42 +0100

    Add argument parsing to main script
    Simplified main script and rewrote it to use arguments parsed to script.

--- src/main.py
+++ src/main.py
@@ -4,41 +4,81 @@ import data
 import modeling
 import analysis
 
+import argparse
+
 from numpy import random
 
-def main():
+random.seed(42)
+
+def main(file_name, latent_size, hidden_structure, filtering_method = None,
+    splitting_method = "random", splitting_fraction = 0.8,
+    feature_selection = None, feature_size = None,
+    number_of_epochs = 100, batch_size = 100, learning_rate = 1e-3,
+    force_training = False):
     
-    latent_size = 50
-    hidden_structure = [128]
+    # Data
     
-    file_name = "GSE63472_P14Retina_merged_digital_expression"
+    training_set, validation_set, test_set = data.loadDataSets(file_name,
+        filtering_method, splitting_method, splitting_fraction,
+        feature_selection, feature_size
+    )
     
-    random.seed(42)
+    # Model
     
-    # data_set = data.loadData(file_name)
-    data_set = data.createSampleData(1000, 500, p = .95)
+    feature_size = training_set.shape[1]
     
-    analysis.analyseData(data_set, name = "sample")
+    model = modeling.VAE(feature_size, latent_size, hidden_structure)
     
-    training_set, validation_set, test_set = data.splitData(data_set)
+    if force_training:
+        model.train(training_set, validation_set, N_epochs = 100)
+        model.save(name = "test")
+    else:
+        model.load(model_name = "test")
     
-    feature_shape = data_set.shape[1]
+    # Analysis
     
-    model = modeling.VAE(feature_shape, latent_size, hidden_structure)
-    
-    model.train(training_set, validation_set, N_epochs = 50)
-    
-    model.save(name = "test")
+    analysis.analyseModel(model, name = "model")
     
     reconstructed_test_set, sample_set, test_metrics = model.evaluate(test_set)
     print(test_metrics)
-    
-    model.load(model_name = "test")
-    
-    reconstructed_test_set, sample_set, test_metrics = model.evaluate(test_set)
-    print(test_metrics)
-    
-    analysis.analyseModel(test_set, reconstructed_test_set, sample_set)
+
+    analysis.analyseResults(test_set, reconstructed_test_set, sample_set, name = "results")
+
+parser = argparse.ArgumentParser(
+    description='Model gene counts in single cells.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+parser.add_argument("--file-name", metavar = "name", type = str, help = "data file name")
+parser.add_argument("--latent-size", metavar = "size", type = int,
+    help = "size of latent space")
+parser.add_argument("--hidden-structure", metavar = "sizes", nargs = '+',
+    type = int, help = "structure of hidden layers")
+parser.add_argument("--filtering-method", metavar = "method", type = str,
+    help = "method for filtering examples")
+parser.add_argument("--splitting-method", metavar = "method", type = str,
+    default = "random", 
+    help = "method for splitting data into training,   validation, and test sets")
+parser.add_argument("--splitting-fraction", metavar = "fraction", type = float,
+    default = 0.8,
+    help = "fraction to use when splitting data into training, validation, and test sets")
+parser.add_argument("--feature-selection", metavar = "selection", type = str,
+    help = "selection of features to use")
+parser.add_argument("--feature-size", metavar = "size", type = int,
+    help = "size of feature space")
+parser.add_argument("--number-of-epochs", metavar = "N", type = int, default = 100,
+    help = "number of epochs for which to train")
+parser.add_argument("--batch-size", metavar = "B", type = int, default = 100,
+    help = "batch size used when training")
+parser.add_argument("--learning-rate", metavar = "epsilon", type = float,
+    default = 1e-3, help = "learning rate when training")
+parser.add_argument("--force-training", action = "store_true",
+    help = "train model whether or not it was previously trained")
 
 if __name__ == '__main__':
-    main()
+    
+    # file_name = "GSE63472_P14Retina_merged_digital_expression"
+    # latent_size = 50
+    # hidden_structure = [500]
+    
+    arguments = parser.parse_args()
+    main(**vars(arguments))
